2aac0ca98d4885684ab2b297484ff59350fee527
[samuel gregory] <[samuelgregory455@gmail.com]>
2025-05-05 09:11:07 +0000

    overlay feature

---
+++ week6/menu/javascript/script.js
@@ -0,0 +1,33 @@
+function subject(subjectName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(subjectName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+  // Navigation Overlay
+  /* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("thisNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("thisNav").style.width = "0%";
+}
