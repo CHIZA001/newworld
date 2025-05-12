b84c74f9646db397797fe059bf802977d40607c6
[samuel gregory] <[samuelgregory455@gmail.com]>
2025-05-12 11:05:35 +0000

    updating root index for image folder

---
+++ week7/scripts/script.js
@@ -0,0 +1,18 @@
+// function toggleMenu() {
+//     const menu = document.getElementById("menuContents")
+//     const overlay = document.getElementById("overlay")
+
+//     const isOpen = menu.classList.contains("show")
+//     if (isOpen) {
+//       menu.classList.remove("show")
+//       overlay.classList.remove("show")
+//     } else {
+//       menu.classList.add("show")
+//       overlay.classList.add("show")
+//     }
+//   }
+
+//   function closeMenu() {
+//     document.getElementById("menuContents").classList.remove("show")
+//     document.getElementById("overlay").classList.remove("show")
+//   }
